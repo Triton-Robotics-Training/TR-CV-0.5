2bad1a201c1e6a8cfc041ed96c933ff5a63f83ce
Daniel Ticau <daniticau@gmail.com>
2026-09-01 01:39:21 +0000

    training 0.5

---
+++ build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/danivm/TR-Autonomy-0.5-Daniel-Ticau/src/cpp_pubsub
+# Build directory: /home/danivm/TR-Autonomy-0.5-Daniel-Ticau/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
